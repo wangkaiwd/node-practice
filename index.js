0e6405efa7ae4747102d182681f9a5c4f9db27bc
wangkaiwd <1162486176@qq.com>
2019-10-13 00:07:04 +0800

    feat: 获取命令中的所有参数 <xxx...>

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const program = require('commander');
-const add = (a) => {
-  console.log('a', a);
-};
-const clear = (c) => {
-  console.log('c', c);
-};
+
 program
-  .option('add <taskName>', 'add a task', add)
-  .option('clear', 'clear all tasks', clear);
+  .option('-x, --xxx', 'what the x');
+
+// Command implemented using action handler (description is supplied separately to `.command`)
+// Returns new command for configuring.
+program
+  .command('add <taskName...>')
+  .description('add a task')
+  .action((a) => {
+    console.log('add command called', a.join(' '));
+  });
+
+program
+  .command('clear')
+  .description('clear task')
+  .action(() => {
+    console.log('clear');
+  });
 
 program.parse(process.argv);
 console.log('stuff');
 
-// 1. 如何找到示例代码
-// 2. 如何进行进一步的开发
+// 操作过程：
+// 1. 随便找一段示例代码(不要太简单)在编辑器里运行
+// 2. 将复制过来的代码根据CRM方法进行学习
+// 3. 为什么想到了添加子命令？
+
