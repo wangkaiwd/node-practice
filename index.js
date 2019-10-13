02cf7ffa9e233e84dd87a0027f1f56d272e6fff2
wangkaiwd <1162486176@qq.com>
2019-10-13 18:19:57 +0800

    feat: 使用Promise再实现同样的代码

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ const db = require('./db');
 const add = async (title) => {
   title = title.join(' ');
   // 想要的调用方式：
-  const list = await db.read();
-  list.push({ title, done: false });
-  await db.write(list);
+  // const list = await db.read();
+  // list.push({ title, done: false });
+  // await db.write(list);
+  db.read()
+    .then(list => {
+      list.push({ title, done: false });
+      return db.write(list);
+    });
+
+  // 分别用Promise和async和await来进行处理结果
+  // 相对来说，async和await会看起来更加优雅
 };
 
 module.exports = add;
