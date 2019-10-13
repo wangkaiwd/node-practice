ec4845c2260a5fef824bd15b804962ce9750bdf3
wangkaiwd <1162486176@qq.com>
2019-10-13 18:05:40 +0800

    feat: 删除旧有的代码

---
+++ cli.js
@@ -0,0 +1,31 @@
+const program = require('commander');
+const add = require('./index');
+program
+  .option('-x, --xxx', 'what the x');
+
+// Command implemented using action handler (description is supplied separately to `.command`)
+// Returns new command for configuring.
+program
+  .command('add <taskName...>')
+  .description('add a task')
+  .action(add);
+
+program
+  .command('clear')
+  .description('clear task')
+  .action(() => {
+    console.log('clear');
+  });
+
+program.parse(process.argv);
+console.log('stuff');
+
+// 操作过程：
+// 1. 随便找一段示例代码(不要太简单)在编辑器里运行
+// 2. 将复制过来的代码根据CRM方法进行学习
+// 3. 为什么想到了添加子命令？
+
+// 新增一个任务：
+// 1. 创建一个add方法，将传入的任务添加到数据库中
+//    1. 用一个文件来充当数据库，将对应的todos列表进行存储
+//    2. 使用home目录下的.todo来作为数据存储的路径
--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const program = require('commander');
+const fs = require('fs');
+const path = require('path');
+const homedir = require('os').homedir();
+const dbPath = path.resolve(homedir, './.todo');
+const add = (title) => {
+  // 想要的调用方式：
+  // const list = db.read()
+  // list.push(task)
+  // db.write(list)
+};
 
-program
-  .option('-x, --xxx', 'what the x');
-
-// Command implemented using action handler (description is supplied separately to `.command`)
-// Returns new command for configuring.
-program
-  .command('add <taskName...>')
-  .description('add a task')
-  .action((a) => {
-    console.log('add command called', a.join(' '));
-  });
-
-program
-  .command('clear')
-  .description('clear task')
-  .action(() => {
-    console.log('clear');
-  });
-
-program.parse(process.argv);
-console.log('stuff');
-
-// 操作过程：
-// 1. 随便找一段示例代码(不要太简单)在编辑器里运行
-// 2. 将复制过来的代码根据CRM方法进行学习
-// 3. 为什么想到了添加子命令？
-
+module.exports = add;
