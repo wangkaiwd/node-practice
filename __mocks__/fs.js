2c905f65ab986915c61761167eb7b84d18261201
wangkaiwd <1162486176@qq.com>
2019-10-14 23:23:28 +0800

    feat: mock fs.readFile logic

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -1,8 +1,21 @@
 const fs = jest.genMockFromModule('fs');
-
+const _fs = jest.requireActual('fs');
 fs.x = () => {
   console.log('hi');
   return 'xxx';
 };
+const readMock = {};
+fs.setReadMock = (path, error, data) => {
+  readMock[path] = [error, data];
+};
+fs.readFile = (path, options, callback) => {
+  // options选传
+  if (typeof callback === 'undefined') {callback = options;}
+  if (path in readMock) {
+    callback(...readMock[path]);
+  } else {
+    _fs.readFile(path, options, callback);
+  }
 
+};
 module.exports = fs;
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -2,9 +2,11 @@ const db = require('../db');
 const fs = require('fs');
 jest.mock('fs');
 describe('db', () => {
-  it('能读文件', () => {
-    expect(db.read).toBeInstanceOf(Function);
-    expect(fs.x()).toBe('xxx');
+  it('能读文件', async () => {
+    const list1 = [{ title: '任务1', done: false }];
+    fs.setReadMock('/xxx', null, JSON.stringify(list1));
+    const list2 = await db.read('/xxx');
+    expect(list2).toStrictEqual(list1);
   });
   it('能写文件', () => {
     expect(db.read).toBeInstanceOf(Function);
