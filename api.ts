39321f8aae2909c27dad8152f04be6fa58169ace
wangkaiwd <1162486176@qq.com>
2019-11-10 18:31:49 +0800

    feat: completed basic function of todos

--- api.ts
+++ api.ts
@@ -1,6 +1,5 @@
 import db, { TaskProp } from './db';
 import * as inquirer from 'inquirer';
-import { Answers } from 'inquirer';
 
 export const add = async (titles: string[]) => {
   const title = titles.join(' ');
@@ -20,58 +19,81 @@ interface ChoiceProps {
 }
 type Actions = 'markAsCompleted' | 'markAsUnComplete' | 'updateTitle' | 'deleteTask'
 type ActionMap = {
-  [K in Actions]: () => void
+  [K in Actions]: (list: TaskProp[], index: number) => void
 }
-const createPrompt = (message: string, choices: ChoiceProps[], type: string = 'list'): Promise<{ key: number | string }> => {
+const createListPrompt = (message: string, choices: ChoiceProps[]): Promise<{ key: number | string }> => {
   return inquirer
     .prompt([
-      { type, name: 'key', message, choices }
+      { type: 'list', name: 'key', message, choices }
     ]);
 };
-const markAsCompleted = () => {
+const markAsCompleted = (list: TaskProp[], index: number) => {
+  list[index].done = true;
+  db.write(list).then();
+};
+
+const markAsUnComplete = (list: TaskProp[], index: number) => {
+  list[index].done = false;
+  db.write(list).then();
+};
+const updateTitle = (list: TaskProp[], index: number) => {
+  askForNewTask(list).then(title => list[index].title = title);
+};
+const deleteTask = (list: TaskProp[], index: number) => {
+  list.splice(index, 1);
+  db.write(list).then();
+};
+const askForNewTask = (list: TaskProp[]) => {
+  const questions = [{
+    type: 'input',
+    name: 'title',
+    message: '新任务的名称'
+  }];
+  return inquirer.prompt(questions)
+    .then((answers): string => answers.title as string);
+};
+const askForAction = (list: TaskProp[], index: number) => {
+  const actions = [
+    { name: '退出', value: -1 },
+    { name: '已完成', value: 'markAsCompleted' },
+    { name: '未完成', value: 'markAsUnComplete' },
+    { name: '修改标题', value: 'updateTitle' },
+    { name: '删除', value: 'deleteTask' }
+  ];
+  createListPrompt('选择操作', actions).then(
+    answers => {
+      if (answers.key !== -1) {
+        const actionMap: ActionMap = { markAsCompleted, markAsUnComplete, updateTitle, deleteTask };
+        actionMap[(answers.key as Actions)](list, index);
+      }
+    }
+  );
 
 };
 
-const markAsUnComplete = () => {
-
-};
-const updateTitle = () => {
-
-};
-const deleteTask = () => {
-
-};
 export const showAll = async () => {
   const list = await db.read();
   const allChoices = (): ChoiceProps[] => {
     return list.map((task, i) => {
       const taskFlag = task.done ? '[x]' : '[_]';
       const value = i;
-      const name = `${taskFlag} ${i + 1} ${task.title}`;
+      const name = `${taskFlag} ${i + 1}. ${task.title}`;
       return { name, value };
     });
   };
-  createPrompt('请选择要操作的任务?', [
+  createListPrompt('请选择要操作的任务?', [
     { name: '退出', value: -1 },
     ...allChoices(),
     { name: '+ 创建任务', value: -2 },
   ]).then(answers => {
     if (answers.key >= 0) {
-      const actions = [
-        { name: '退出', value: -1 },
-        { name: '已完成', value: 'markAsCompleted' },
-        { name: '未完成', value: 'markAsUnComplete' },
-        { name: '修改标题', value: 'updateTitle' },
-        { name: '删除', value: 'deleteTask' }
-      ];
-      createPrompt('选择操作', actions).then(
-        answers => {
-          if (answers.key !== -1) {
-            const actionMap: ActionMap = { markAsCompleted, markAsUnComplete, updateTitle, deleteTask };
-            actionMap[(answers.key as Actions)]();
-          }
-        }
-      );
+      askForAction(list, answers.key as number);
+    } else if (answers.key === -2) {
+      askForNewTask(list)
+        .then(title => {
+          list.push({ title, done: false });
+          db.write(list);
+        });
     }
   });
 };
